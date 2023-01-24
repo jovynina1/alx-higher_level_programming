682d6cc8df1db1ba824933737148c4635710d457
jovynina1 <jovynina@gmail.com>
2023-01-24 13:57:57 +0000

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" defines a square """
+"""Define a square."""
 
 
 class Square:
-    """ square with private instance attribute size """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
+        """Initialize a new Square
+
+        
         Args:
-            size: size of square
+            size (int): The size of the new square.
         """
 
-        if type(size) is int:
+        if not isinstance(size, int):
             if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-            else:
-                raise TypeError('size must be an integer')
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
